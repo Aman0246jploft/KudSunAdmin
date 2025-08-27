1718462dbfa83ff072c441c4a8010e71f68ae2e4
Aman0246jploft <amakashyap0246jploft@gmail.com>
2025-08-27 15:03:17 +0530

    Save

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,10 +1,5 @@
 // export const baseURL = "http://localhost:9097/api/v1";
 // export const socketURL = "http://localhost:9097";
 
-
-
-export const baseURL = "http://3.109.98.222:9097/api/v1";
-export const socketURL = "http://3.109.98.222:9097";
-
-
-
+export const baseURL = "http://3.109.98.222:9194/api/v1";
+export const socketURL = "http://3.109.98.222:9194";
